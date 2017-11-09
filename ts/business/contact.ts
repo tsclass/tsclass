ac73badd7601e69caecef887799975dfc55f33ce
Philipp Kunz <philkunz@Philipps-MacBook-Pro.local>
2017-11-09 02:16:56 +0100

    update ci

--- ts/business/contact.ts
+++ ts/business/contact.ts
@@ -1,4 +1,17 @@
+export type TContactSalutation = 'Mr' | 'Ms' | 'Mrs'
+
+export type TContactType = 'person' | 'company'
+
+export type TContactTitle =  'Doctor' | 'Professor'
+
 export interface IContact {
+  salutation?: TContactSalutation
+  type: TContactType
+  title: TContactTitle
   name: string
-  
+  streetName: string
+  houseNumber: string
+  postalCode: string
+  city: string
+  country: string
 }
