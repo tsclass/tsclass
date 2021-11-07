cb556f71dadbeb86a619ad24ff14642c60e0375b
Philipp Kunz <gitlab@philkunz.com>
2021-11-07 03:16:41 +0100

    fix(core): update

--- ts/container/container.ts
+++ ts/container/container.ts
@@ -1,5 +1,9 @@
-export interface IContainer {
+export interface IContainerImage {
   registryUrl: string;
   tag: string;
+  /**
+   * version is usually derived from labels
+   */
   version: string;
+  labels: string[];
 }
