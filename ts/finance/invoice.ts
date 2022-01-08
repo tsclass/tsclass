0bb5ee7facdd891281cb60c469c310eff66c7c14
Philipp Kunz <gitlab@philkunz.com>
2022-01-08 04:56:55 +0100

    fix(core): update

--- ts/finance/invoice.ts
+++ ts/finance/invoice.ts
@@ -4,6 +4,7 @@ export type TInvoiceStatus = 'draft' | 'invoice' | 'paid' | 'refunded';
 
 export interface IInvoiceItem {
   name: string;
+  articleNumber?: string;
   unitType: string;
   unitQuantity: number;
   unitNetPrice: number;
@@ -16,6 +17,11 @@ export interface IInvoice {
   billedTo: business.IContact;
   status: TInvoiceStatus;
   items: IInvoiceItem[];
+  periodOfPerformance?: {
+    from: number;
+    to: number;
+  };
+  deliveryDate?: number;
   printResult?: {
     pdfBufferString: string;
     totalNet: number;
