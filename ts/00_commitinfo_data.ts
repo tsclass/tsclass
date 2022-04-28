d672b9973eb571e91d54e231d0b01937c6998592
Philipp Kunz <gitlab@philkunz.com>
2022-04-28 12:20:27 +0200

    fix(core): update

--- ts/00_commitinfo_data.ts
+++ ts/00_commitinfo_data.ts
@@ -2,10 +2,10 @@
  * autocreated commitinfo by @pushrocks/commitinfo
  */
 export const name: string = '@tsclass/tsclass';
-export const version: string = '4.0.48';
+export const version: string = '4.0.1';
 export const description: string = 'common classes for TypeScript'
 export const commitinfo = {
   name: '@tsclass/tsclass',
-  version: '4.0.48',
+  version: '4.0.1',
   description: 'common classes for TypeScript'
 }
